934107a9975b7ed2e7f235f1cd2cace663b30e53
martinoc <martin.o.94@hotmail.com>
2017-04-30 19:32:09 +1000

    timer delay splash done

--- SCaR_Arcade/Start.cs
+++ SCaR_Arcade/Start.cs
@@ -12,7 +12,9 @@ using Android.Widget;
 using Android.Graphics;
 /// <summary>
 /// Creator: Ryan Cunneen
+/// Creator: Martin O'Connor
 /// Student number: 3179234
+/// Student number: 3279660
 /// Date created: 25-Mar-17
 /// Date modified: 30-Apr-17
 /// </summary>
@@ -21,28 +23,62 @@ namespace SCaR_Arcade
     [Activity(
         Label = "Start",
         MainLauncher = true,
+        Icon = "@drawable/SCaRARCADE",
         Theme = "@android:style/Theme.NoTitleBar"
     )]
     public class Start : Activity
     {
+        /*
+       * sources
+       * http://stackoverflow.com/questions/27196590/i-want-create-timer-thread-for-android-with-xamarin
+       * https://developer.xamarin.com/guides/android/user_interface/creating_a_splash_screen/
+       * https://forums.xamarin.com/discussion/58925/how-can-i-set-a-progressbar-in-android-using-c
+      */
         private ImageView appLogoImgView;
         private ProgressBar progress;
+        System.Timers.Timer timer;
+        int delay;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             try
             {
                 base.OnCreate(savedInstanceState);
+
                 SetContentView(Resource.Layout.Start);
+
                 appLogoImgView = FindViewById<ImageView>(Resource.Id.ApplicationLogoImgView);
                 progress = FindViewById<ProgressBar>(Resource.Id.startProgress);
                 Bitmap appLogo = BitmapFactory.DecodeResource(Resources, Resource.Drawable.SCaRARCADE);
-                appLogoImgView.SetImageBitmap(appLogo);
 
-                //BeginActivity(typeof(MainActivity), "", 0);
+                appLogoImgView.SetImageBitmap(appLogo);
+                delay = 3;
+                CountDown();
             }
             catch
             {
                 GlobalApp.Alert(this, 1);
+            }
+        }
+        private void CountDown()
+        {
+
+           timer = new System.Timers.Timer();
+
+            
+            timer.Interval = 1000;
+            timer.Elapsed += OnTimedEvent;
+            timer.Enabled = true;
+
+        }
+        private void OnTimedEvent(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            delay--;
+
+            if (delay == 0)
+            {
+                timer.Dispose();
+                BeginActivity(typeof(MainActivity), "", 0);
             }
         }
         // ----------------------------------------------------------------------------------------------------------------
