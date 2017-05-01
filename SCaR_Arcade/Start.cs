2438fa324b825c3f532c8888388171eb17e6b87d
IgnitionOrk <c3179234@uon.edu.au>
2017-05-01 10:05:18 +1000

    Just added some comments

--- SCaR_Arcade/Start.cs
+++ SCaR_Arcade/Start.cs
@@ -29,11 +29,11 @@ namespace SCaR_Arcade
     public class Start : Activity
     {
         /*
-       * sources
-       * http://stackoverflow.com/questions/27196590/i-want-create-timer-thread-for-android-with-xamarin
-       * https://developer.xamarin.com/guides/android/user_interface/creating_a_splash_screen/
-       * https://forums.xamarin.com/discussion/58925/how-can-i-set-a-progressbar-in-android-using-c
-      */
+        * sources
+        * http://stackoverflow.com/questions/27196590/i-want-create-timer-thread-for-android-with-xamarin
+        * https://developer.xamarin.com/guides/android/user_interface/creating_a_splash_screen/
+        * https://forums.xamarin.com/discussion/58925/how-can-i-set-a-progressbar-in-android-using-c
+        */
         private ImageView appLogoImgView;
         private ProgressBar progress;
         System.Timers.Timer timer;
@@ -46,13 +46,11 @@ namespace SCaR_Arcade
                 base.OnCreate(savedInstanceState);
 
                 SetContentView(Resource.Layout.Start);
-
                 appLogoImgView = FindViewById<ImageView>(Resource.Id.ApplicationLogoImgView);
                 progress = FindViewById<ProgressBar>(Resource.Id.startProgress);
                 Bitmap appLogo = BitmapFactory.DecodeResource(Resources, Resource.Drawable.SCaRARCADE);
-
                 appLogoImgView.SetImageBitmap(appLogo);
-                delay = 3;
+                delay = 3;      // Arbitrary number (3 seconds). 
                 CountDown();
             }
             catch
@@ -60,21 +58,18 @@ namespace SCaR_Arcade
                 GlobalApp.Alert(this, 1);
             }
         }
+        // Initiates a timer. 
         private void CountDown()
         {
-
-           timer = new System.Timers.Timer();
-
-            
+            timer = new System.Timers.Timer();            
             timer.Interval = 1000;
             timer.Elapsed += OnTimedEvent;
             timer.Enabled = true;
-
         }
+        // Event handler
         private void OnTimedEvent(object sender, System.Timers.ElapsedEventArgs e)
         {
             delay--;
-
             if (delay == 0)
             {
                 timer.Dispose();
