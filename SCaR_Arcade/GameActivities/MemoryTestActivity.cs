c23d80835b3d2997c35696007a314e5be79b9262
Saxoncs <saxon.cs@outlook.com>
2017-05-02 19:02:58 +1000

    Added most of the game logic (commented out)
    Added most of the logic for the running of the base game, It's currently
    commented out while I work on how to perform some specific tasks on a
    mobile device as opposed to a PC

--- SCaR_Arcade/GameActivities/MemoryTestActivity.cs
+++ SCaR_Arcade/GameActivities/MemoryTestActivity.cs
@@ -27,23 +27,149 @@ namespace SCaR_Arcade.GameActivities
    ]
     public class MemoryTestActivity : Activity
     {
-        // Instances for Memory Test
-        //----------------
-        //----------------
-        //----------------
-    
+
+        // firstClicked points to the first button control 
+        // that the player clicks, it must be null 
+        // if the player hasn't clicked a label yet
+        Button firstClicked = null;
+
+        // secondClicked points to the second button control 
+        // that the player clicks
+        Button secondClicked = null;
+
+        // Use this Random object to choose random icons for the squares
+        Random random = new Random();
+
+        // Each of these letters is an interesting icon
+        // in the Symbols1 font,
+        // and each icon appears twice in this list
+        List<string> icons = new List<string>()
+        {
+            "T", "T", "I", "I", "A", "A", "S", "S",
+            "J", "J", "V", "V", "N", "N", "Q", "Q"
+        };
+
+        public MemoryTestActivity()
+        {
+            //InitializeComponent();
+
+            //AssignIconsToSquares();
+        }
+
         protected override void OnCreate(Bundle bundle)
         {
+        //    // Timer1 relates to the delay when an incorrect selection is made
+        //    // and thereforeis only on after two non-matching 
+        //    // icons have been shown to the player, 
+        //    // so ignore any clicks if the timer is running
+        //    if (timer1.Enabled == true)
+        //        return;
+
+        //    Button clickedButton = sender as Button;
+
+        //    if (clickedButton != null)
+        //    {
+
+        //        //If the clicked label is black, the player clicked
+        //        //an icon that's already been revealed, ignore it
+        //        if (clickedButton.ForeColor == Color.Black)
+        //            return;
+
+        //        // If firstClicked is null, this is the first icon 
+        //        // in the pair that the player clicked,
+        //        // so set firstClicked to the label that the player 
+        //        // clicked, change its color to black, and return
+        //        if (firstClicked == null)
+        //        {
+        //            firstClicked = clickedButton;
+        //            //firstClicked.ForeColor = Color.Black;
+
+        //            return;
+        //        }
+
+        //        // If the player gets this far, the timer isn't
+        //        // running and firstClicked isn't null,
+        //        // so this must be the second icon the player clicked
+        //        // Set its colour to match the background colour
+        //        //secondClicked = clickedLabel;
+        //        //secondClicked.ForeColor = Color.Black;
+
+        //        // Check to see if the player won
+        //        CheckForWinner();
+
+        //        // If the player clicked two matching icons, keep them 
+        //        // black and reset firstClicked and secondClicked 
+        //        // so the player can click another icon
+        //        if (firstClicked.Text == secondClicked.Text)
+        //        {
+        //            firstClicked = null;
+        //            secondClicked = null;
+        //            return;
+        //        }
+
+        //        // If the player gets this far, the player 
+        //        // clicked two different icons, so start the 
+        //        // timer (which will wait three quarters of 
+        //        // a second, and then hide the icons)
+        //       // timer1.Start();
+        //    }
+        //}
+
+        ///// This timer is started when the player clicks 
+        ///// two icons that don't match,
+        ///// so it counts three quarters of a second 
+        ///// and then turns itself off and hides both icons
+        //private void timer1_Tick(object sender, EventArgs e)
+        //{
+        //    // Stop the timer
+        //   // timer1.Stop();
+
+        //    // Hide both icons
+        //    //firstClicked.ForeColor = firstClicked.BackColor;
+        //    //secondClicked.ForeColor = secondClicked.BackColor;
+
+        //    // Reset firstClicked and secondClicked 
+        //    // so the next time a label is
+        //    // clicked, the program knows it's the first click
+        //    firstClicked = null;
+        //    secondClicked = null;
+        //}
+
+        ///// Check every icon to see if it is matched, by 
+        ///// comparing its foreground color to its background color. 
+        ///// If all of the icons are matched, the player wins
+        //private void CheckForWinner()
+        //{
+        //    // Go through all of the labels in the TableLayoutPanel, 
+        //    // checking each one to see if its icon is matched
+        //    foreach (Control control in tableLayoutPanel1.Controls)
+        //    {
+        //        Label iconLabel = control as Label;
+
+        //        if (iconLabel != null)
+        //        {
+        //            if (iconLabel.ForeColor == iconLabel.BackColor)
+        //                return;
+        //        }
+        //    }
+
+        //    // If the loop didn’t return, it didn't find
+        //    // any unmatched icons
+        //    // That means the user won. Show a message and close the form
+        //    MessageBox.Show("You matched all the icons!", "Congratulations");
+        //    Close();
+
             try
             {
                 base.OnCreate(bundle);
 
                 SetContentView(Resource.Layout.MemoryTest);
 
-                var GameBoard = FindViewById<GridView>(Resource.Id.GameBoard);
-                GameBoard.Adapter = new ImageAdapter(this);
 
-                GameBoard.ItemClick += delegate (object sender, AdapterView.ItemClickEventArgs args) {
+                var GameBoard = FindViewById<GridView>(Resource.Id.GameBoard);
+
+                GameBoard.ItemClick += delegate (object sender, AdapterView.ItemClickEventArgs args)
+                {
                     Toast.MakeText(this, args.Position.ToString(), ToastLength.Short).Show();
 
                 };
@@ -54,68 +180,6 @@ namespace SCaR_Arcade.GameActivities
             }
         }
 
-        public class ImageAdapter : BaseAdapter
-        {
-            Context context;
-
-            public ImageAdapter(Context c)
-            {
-                context = c;
-            }
-
-            public override int Count
-            {
-                get { return thumbIds.Length; }
-            }
-
-            public override Java.Lang.Object GetItem(int position)
-            {
-                return null;
-            }
-
-            public override long GetItemId(int position)
-            {
-                return 0;
-            }
-
-            // create a new ImageView for each item referenced by the Adapter
-            public override View GetView(int position, View convertView, ViewGroup parent)
-            {
-                ImageView imageView;
-
-                if (convertView == null)
-                {  // if it's not recycled, initialize some attributes
-                    imageView = new ImageView(context);
-                    imageView.LayoutParameters = new GridView.LayoutParams(85, 85);
-                    imageView.SetScaleType(ImageView.ScaleType.CenterCrop);
-                    imageView.SetPadding(8, 8, 8, 8);
-                }
-                else
-                {
-                    imageView = (ImageView)convertView;
-                }
-
-                imageView.SetImageResource(thumbIds[position]);
-                return imageView;
-            }
-
-            // references to our images
-            int[] thumbIds = {
-        
-                //Add pathways to images for the game here, eg:
-        //Resource.Drawable.sample_2, Resource.Drawable.sample_3,
-        //Resource.Drawable.sample_4, Resource.Drawable.sample_5,
-        //Resource.Drawable.sample_6, Resource.Drawable.sample_7,
-        //Resource.Drawable.sample_0, Resource.Drawable.sample_1,
-        //Resource.Drawable.sample_2, Resource.Drawable.sample_3,
-        //Resource.Drawable.sample_4, Resource.Drawable.sample_5,
-        //Resource.Drawable.sample_6, Resource.Drawable.sample_7,
-        //Resource.Drawable.sample_0, Resource.Drawable.sample_1,
-        //Resource.Drawable.sample_2, Resource.Drawable.sample_3,
-        //Resource.Drawable.sample_4, Resource.Drawable.sample_5,
-        //Resource.Drawable.sample_6, Resource.Drawable.sample_7
-                            };
-        }
-
     }
+
 }
