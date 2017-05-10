5a5be86678f383a7502b134bdc35d0fa304a6de2
martinoc <martin.o.94@hotmail.com>
2017-05-10 12:47:46 +1000

    .

--- SCaR_Arcade/GlobalApp.cs
+++ SCaR_Arcade/GlobalApp.cs
@@ -10,8 +10,10 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 /// <summary>
-/// Created by: Ryan Cunneen
+/// Creator: Ryan Cunneen
+/// Creator: Martin O'Connor
 /// Student number: 3179234
+/// Student number: 3279660
 /// Date created: 20-Apr-2017
 /// Date modified: 20-Apr-2017
 /// </summary>
